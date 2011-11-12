2aebdbee3ca671284e462e376068f6737b3e8a64
“mibennet” <mibennettc@gmail.com>
2011-11-12 00:09:14 -0400

    Autoload: Añadidos helpers de url y form, ademas del modelo. Model_model: Clase para el modelo.

--- app/application/config/autoload.php
+++ app/application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('url','form');
 
 
 /*
@@ -109,7 +109,7 @@ $autoload['language'] = array();
 |
 */
 
-$autoload['model'] = array();
+$autoload['model'] = array('model_model');
 
 
 /* End of file autoload.php */

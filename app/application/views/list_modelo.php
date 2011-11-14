2d8ab0ef519df90908a3be6e3d9b828862c79c0e
Ioachim <krucisdk@gmail.com>
2011-11-13 23:51:36 -0300

    Configuracion modificada para acceder a archivos de php en cualquier path, y conectar php5-fpm por unix sockets en vez de TCP

--- app/application/views/list_modelo.php
+++ app/application/views/list_modelo.php
@@ -5,7 +5,12 @@
 </head>
 <body>
 <h1>Modelos</h1>
+<?php echo anchor('modelo/add', 'crear modelo') ?>
 <table>
+	<tr>
+		<th>nombre</th>
+		<th></th>
+	</tr>
 <?php foreach($modelo as $m): ?>
 	<tr>
 		<td><?php echo anchor('modelo/view/' . $m['nombre'], $m['nombre']); ?></td>

21e68d0d0847a58a21054c953f624c5b7e247340
JimmyRoX <Jimmyraynor@gmail.com>
2011-11-15 14:04:56 -0300

    Agregando controlador y modelo basico. Ademas mi computador indexo todos los archivos por lo que aparecen todos modificados -.-''

---
+++ app/application/controllers/widget.php
@@ -0,0 +1,10 @@
+<?php
+class Widget extends CI_Controller
+{
+	function index()
+	{
+		$this->load->view('class_widget');
+
+	}
+}
+
---
+++ app/application/views/class_widget.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="<?php echo base_url();?>css/jquery-ui-1.8.16.custom.css" type="text/css" media="screen"/>
+    <title>Untitled Page</title>
+</head>
+<body>
+
+<h2>Prueba!</h2>
+
+</body>
+</html>
+

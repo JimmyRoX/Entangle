97ee7d473a22f5d81cb7eb36ec3f3af91d13d4c8
JimmyRoX <Jimmyraynor@gmail.com>
2011-12-05 21:44:47 -0300

    Archivos para subir y procesar widgets, ajax usando para algunas cosas

--- app/application/controllers/widget.php
+++ app/application/controllers/widget.php
@@ -8,11 +8,35 @@ class Widget extends CI_Controller
 	}
 	function content() 
 	{
-		$this->load->view('content_widget');
+		if(($data['widget_file'] = $this->input->post('widget_file')) != "") {
+			$this->load->helper('widget_parser');			
+			$this->load->view('content_widget', $data);	
+		}
+		else $this->load->view('content_widget');
+
 	}
 	function popup()
 	{
 		$this->load->view('popup_widget');
+	}
+	function subject() 
+	{
+		$this->load->view('subject_widget');
+	}
+	function asdf() {
+		$this->load->view('subject_widget');
+	}
+	function upload() {
+		$config['upload_path'] = './uploads/';
+		$config['allowed_types'] = 'html';
+		$this->load->library('upload', $config);
+				
+		$this->upload->do_upload();
+		$data = array('upload_data' => $this->upload->data());
+		$data['widget_file'] = $data['upload_data']['full_path'];
+		$data['widget_type'] = $this->input->post('widget_type');
+
+		$this->load->view('class_widget', $data);
 	}
 }
 
--- app/application/helpers/widget_parser_helper.php
+++ app/application/helpers/widget_parser_helper.php
@@ -1,20 +1,20 @@
 <?php
-//if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 
 function widget_to_php($widget_string) {
 	
 	//SETEO DE STRINGS PARA OBTENER DATOS
 	$parser_nombre_submodelo = '"name_submodel"';
-	$parser_metadata = '"meta: $1"';
+	$parser_metadata = '"metadata $1"';
 	$parser_link_submodelo = '"link_submodel"';
 	//FIN DE SETEO, NO TOCAR EL RESTO
 
 
 	//Agrego tags php a estas variables
-	$parser_metadata = '<?php echo '.$metadata.'; ?>';
-	$parser_link_submodelo = '<?php echo '.$link_submodelo.'; ?>';
-	$parser_nombre_submodelo = '<?php echo '.$nombre_submodelo.'; ?>';
+	$parser_metadata = '<?php echo '.$parser_metadata.'; ?>';
+	$parser_link_submodelo = '<?php echo '.$parser_link_submodelo.'; ?>';
+	$parser_nombre_submodelo = '<?php echo '.$parser_nombre_submodelo.'; ?>';
 
 	//eliminar todo el php existente
 	$regex_pattern = '/(<\?{1}[pP\s]{1}.+\?>)/';
@@ -23,11 +23,12 @@ function widget_to_php($widget_string) {
 
 	//reemplazar tags de metadata por php
 	$regex_pattern = array('/\[METADATA=([^\]]+)\]/', '/\[LINK_SUBMODELO\]/' ,  '/\[NOMBRE_SUBMODELO\]/');
-	$regex_replace = array($metadata,$link_submodelo, $nombre_submodelo);
+	$regex_replace = array($parser_metadata,$parser_link_submodelo, $parser_nombre_submodelo);
 	$str = preg_replace($regex_pattern, $regex_replace, $str);
 
 	return $str;
 }
+
 
 
 

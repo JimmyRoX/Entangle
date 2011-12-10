69462935ed070591331cf7b4a9efdf44c96b1ffa
“mibennet” <mibennettc@gmail.com>
2011-12-10 01:08:54 -0400

    - Se cambio Add Reference para que apareciera en la lista de contribuciones. - Ahora tambien se pueden crear contribuciones al momento de agregar una referencia. - Se añadio css a la vista de subir contribuciones

--- app/application/views/add_contribucion.php
+++ app/application/views/add_contribucion.php
@@ -2,44 +2,122 @@
 <html>
 <head>
     <title>Subir Contribuci&oacute;n</title>
+    <link href="<?php echo base_url('css/style.css') ?>" rel="stylesheet" type="text/css" media="screen" />
 </head>
 <body>
-    <script type="text/javascript" src="<?php echo base_url('script/jquery-1.7.min.js') ?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('script/contribucionform.js') ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('script/jquery-1.7.min.js') ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('script/contribucionform.js') ?>"></script>
 
-    <form id="contrib_form" action="<?php echo base_url('contribution/add') ?>" method="post">
-    <input type="hidden" value="false" name="is_file" id="is_file" />
-    <h1>Subir Contribuci&oacute;n</h1>
-    <p>
-        <label>
-            SubModelo
-            <select name="submodel" id="submodel" required>
-		<option selected="selected">Elije Submodelo...</option>
-            <?php foreach($submodels as $submodel): ?>
-                <option value="<?php echo($submodel['id']); ?>"><?php echo($submodel['nombre']); ?></option>
-            <?php endforeach; ?>
-            </select>
-        </label>
-    </p>
+	<div id="logo">
+		<h1><a href="index.php">Entangle </a></h1>
+		<p><em> EL lugar para compartir apuntes</em></p>
+	</div>
+	<hr />
+	<!-- end #logo -->
+	<div id="header">
 
-    <p>
-        <label>
-            Tipo de contribucion
-            <select name="tipoContrib" id="tipoContrib" required>
-		<option selected="selected">Elije el tipo de contribuci&oacute;n...</option>
-            </select>
-        </label>
-    </p>
-    <p>
-        <label id="content_label">
-            contenido
-        </label>
-    </p>
-    <div class="metadata" id="metadata">
-    </div>
 
-    </form>
-    <input type="submit" name="add" value="Subir">
+		<div id="search">
+			
+			<?php echo form_open('search/search_result') ?>
+				<fieldset>				
+				<input id="search-text" size="15" name="contribucion" type=text/>
+				<input type="submit"  id="search-submit" value="GO" />				 
+				 
+				</fieldset>
+	
+			<?php echo form_close(); ?>	
+		</div>
+		<!-- end #search -->
+		<div id="menu">
+			<ul>
+				<li><a href="mailto:ralarcon@ing.puc.cl">Contacto</a></li>
+			</ul>
+					<!-- end #menu -->
+		</div>
+		
+	</div>
+	<!-- end #header -->
+
+	<div id="page">
+		<div id="content">
+		  <div class="post">
+				<h2 class="title"><a href="#">Nueva Contribucion </a></h2>
+
+	<div id="contrib">
+		<form id="contrib_form" action="<?php echo base_url('contribution/add') ?>" method="post">
+		<input type="hidden" value="false" name="is_file" id="is_file" class="is_file"/>
+		<p>
+		<label>
+		    SubModelo
+		    <select name="submodel" id="submodel" required>
+			<option selected="selected">Elije Submodelo...</option>
+		    <?php foreach($submodels as $submodel): ?>
+			<option value="<?php echo($submodel['id']); ?>"><?php echo($submodel['nombre']); ?></option>
+		    <?php endforeach; ?>
+		    </select>
+		</label>
+		</p>
+
+		<p>
+		<label>
+		    Tipo de contribucion
+		    <select name="tipoContrib" id="tipoContrib" required>
+			<option selected="selected">Elije el tipo de contribuci&oacute;n...</option>
+		    </select>
+		</label>
+		</p>
+		<p>
+		<label class="content_label">
+		</label>
+		</p>
+		<div class="metadata" id="metadata">
+		</div>
+		<input type="submit" name="add" value="Subir">
+		</form>
+	</div>
+
+	</div>
+	</div>
+	<!--end content-->
+		<div id="sidebar">
+			<ul>
+				<li>
+					<h2>Acerca</h2>
+					<p>Entangle se basa en los usuarios para poder compartir información acerca de ramos de manera transpartente</p>
+				</li>
+				<li>
+					<h2>Subir Contribución </h2>
+					<ul>
+						<li><a href="submodelo">Nueva Contribución</a></li>
+
+
+					</ul>
+				</li>
+				<li>
+					<h2>Sitios de Interes </h2>
+					<ul>
+						<li><a href="www.uc.cl">Página UC</a></li>
+						<li><a href="www.caiuc.cl">Sitio Web CAI</a></li>
+						<li><a href="www.ing.puc.cl">Ing.puc.cl</a></li>
+						<li><a href="www.entangle.cl">Entangle</a></li>
+
+					</ul>
+				</li>
+				
+				
+			</ul>
+		</div>
+		<!-- end #sidebar -->
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+	<!-- end #page -->
+	<div id="footer">
+		<p>Copyright (c) 2011 Entangle.cl All rights reserved. Design by <a href="www.tomas.icaza.com">T.i</a>.</p>
+	</div>
+	<!-- end #footer -->
+</body>
+</html>
 
 </body>
 </html>
--- app/script/contribucionform.js
+++ app/script/contribucionform.js
@@ -1,16 +1,18 @@
 $(function(){
 
-
 $("select#submodel option[selected]").removeAttr("selected");
 $("select#submodel option[value='']").attr("selected", "selected");
 $("#content_label").hide();
+
+$("select#submodel_list option[selected]").removeAttr("selected");
+$("select#submodel_list option[value='']").attr("selected", "selected");
 
 $("#submodel").change(function(){
 	var id = $("#submodel").val();
 
 	//Hacemos reset de los campos
 	$("#content_label").hide();	
-	$("#metadata").html("");	
+	$("#contrib .metadata").html("");	
 	$("#tipoContrib").html('<option selected="selected">Elije el tipo de contribución</option>');
 	
 	$.get("../../modelo/contribuciones_json/", { 'submodel_id': id},
@@ -29,96 +31,179 @@ $("#tipoContrib").change(function(){
 	var type = $("#tipoContrib").val();
 
 	//Resetiamos los campos
-	$("#metadata").html("");
+	$("#contrib .metadata").html("");
 	$("#content_label").hide();
 
 	$.get("../../modelo/contribucion_json/", { 'submodel_id': $("#submodel").val(), 'contrib_nombre': type},
 	 function(data){
-	   updateForm(data);
+	   updateForm($("#contrib"),$("form#contrib_form"), "metadata", data);
 	 }, "json");
 
 
 });
 
-function updateForm(contrib)
+function updateForm(container, form, arrayname,  contrib)
 {
-	
 	if(contrib.content == "longtext")
 	{
-		$("#content_label").html('contenido <textarea name="content" required />');
-		$( "form#contrib_form" )
+		$(form).find(".content_label").html('Contenido <textarea name="content" required />');
+		$(form)
 			.removeAttr( "enctype")
 			.removeAttr( "encoding");
-		$("#is_file").val("false");
+		$(form).find(".is_file").val("false");
 	}
 	else if(contrib.content == "url")
 	{
-		$("#content_label").html('contenido <input type="url" name="content" required />');
-		$( "form#contrib_form" )
+		$(form).find(".content_label").html('Contenido <input type="url" name="content" required />');
+		$(form)
 			.removeAttr( "enctype")
 			.removeAttr( "encoding");
-		$("#is_file").val("false");
+		$(form).find(".is_file").val("false");
 	}
 	else
 	{
-		$("#content_label").html('contenido <input type="file" name="content" required />');
-		$( "form#contrib_form" )
+		$(form).find(".content_label").html('Contenido <input type="file" name="content" required />');
+		$(form)
 			.attr( "enctype", "multipart/form-data" )
 			.attr( "encoding", "multipart/form-data" );
-		$("#is_file").val("true");
+		$(form).find(".is_file").val("true");
 		
 	}
-	$("#content_label").show();
+	$(form).find("#content_label").show();
 
 	//Actualizamos los campos de metadata
-	$("#metadata").html("");
-	jQuery.each(contrib.metadata, function(i, val) {
+	$(container).find(".metadata:first").html("");
 
-		addMetadataField(val.name, val.tipo);
+	jQuery.each(contrib.metadata, function(i, val) {
+		
+		addMetadataField($(container).find(".metadata:first"), arrayname,val.name, val.tipo);
 
 	});
 
 }
 
-function addMetadataField(name, type)
+function addMetadataField(selector, arrayname, name, type)
 {
 	if(type == "string")
-		$("#metadata").append('<p><label>'+name+'</label> <input type="text" name="metadata['+
+		$(selector).append('<p><label>'+name+'</label> <input type="text" name="'+arrayname+'['+
 		name+']" required /></p>');
 	else if(type == "longtext")
-		$("#metadata").append('<p><label>'+name+'</label> <textarea name="metadata['+
+		$(selector).append('<p><label>'+name+'</label> <textarea name="'+arrayname+'['+
 		name+']" required /></p>');
 	else if(type == "url")
-		$("#metadata").append('<p><label>'+name+'</label> <input type="url" name="metadata['+
+		$(selector).append('<p><label>'+name+'</label> <input type="url" name="'+arrayname+'['+
 		name+']" required /></p>');
 	else if(type == "number")
-		$("#metadata").append('<p><label>'+name+'</label> <input type="number" name="metadata['+
+		$(selector).append('<p><label>'+name+'</label> <input type="number" name="'+arrayname+'['+
 		name+']" required /></p>');
 	else if(type == "date")
-		$("#metadata").append('<p><label>'+name+'</label> <input type="date" name="metadata['+
+		$(selector).append('<p><label>'+name+'</label> <input type="date" name="'+arrayname+'['+
 		name+']" required /></p>');
 }
 
-$("#tipoRef").change(function(){
-	//Obtenemos el tipo de referencia
-	var type = $("#tipoRef").val();
 
-	$.get("../../contribution/reference_json/"+$("#contrib").val(), { 'tipoRef': type},
+
+$("#submodel_list").change(function(){
+	var id = $("#submodel_list").val();
+
+	$.get("../../contribution/contributions_json/", { 'submodel_id': id},
 	 function(data){
-		$("#target").html('<option selected="selected">Elije la contribución objetivo...</option>');
-		jQuery.each(data.contribs, function(i, val){
-			$("#target").append("<option value="+val.id+">"+val.name+"</option>");
+	   $("#contribs").html("Contribuciones");
+	   jQuery.each(data, function(i, val){
+		$("#contribs").append('<div id="contrib_'+val.id+
+		'"><a href="../../contribution/view/'+val.id+'">'+
+		val.nombre+'</a><input type="image" src="../../images/imgres-1.jpeg" width="20" height="20" onclick="window.location.href=\'../../contribution/delete/'+val.id+
+		'\'" value="Remove"><input type="button" class="addReferenceButton" value="Add Ref"/></div>');
+		$("#contrib_"+val.id+" .addReferenceButton").click(function(){ addReferenceHandler(val.id); });
+	  	
+	   });
+	 }, "json");
+	});
+
+
+function addReferenceHandler(id)
+{
+	var contribDiv = $("#contrib_"+id);
+	$("#form_container").remove();
+	$.get("../../contribution/references_json", { 'contribution_id': id},
+	 function(data){
+
+		if(data == ""){
+			contribDiv.append("<div id='form_container'>No es posible agregarle referencias.</p>");
+			return;
+		}
+		var container = $("#prototype").clone();
+		container.attr("id", "form_container");	
+		container.find(".contrib_id").val(id);
+		container.find(".tipoRef").html('<option selected="selected" value="-1">Elije el tipo de referencia...</option>');
+		jQuery.each(data, function(i, val){
+			container.find(".tipoRef").append("<option value='"+val.name+"' data-target='"+
+				val.target+"'>"+val.name+"</option>");
 		});
-		$("#metadata").html("");
+		
+		
+		contribDiv.append(container);
+		container.show();
+		contribDiv.find(".submodel").val($("#submodel_list").val());
+
+		container.find(".tipoRef").change(function(){changeTipoRefHandler(id);});
+
+	 }, "json");
+	
+	 
+}
+
+function changeTipoRefHandler(c_id){
+
+	var contribDiv = $("#contrib_"+c_id);
+
+	//Obtenemos el tipo de referencia
+	var type = contribDiv.find(".tipoRef").val();
+
+	//Obtenemos el tipo de contribucion target de dicha referencia
+	var target = contribDiv.find(".tipoRef").find("option:selected").data("target");
+
+	if(type == "-1")
+		contribDiv.find(".newcontrib").hide();
+
+	$.get("../../contribution/reference_json/"+c_id, { 'tipoRef': type, "target":target},
+	 function(data){
+		contribDiv.find(".target").html('<option selected="selected" value="-1">Nueva Contribuci&oacute;n...</option>');
+		jQuery.each(data.contribs, function(i, val){
+		contribDiv.find(".target").append("<option value='"+val.id+"'>"+val.name+"</option>");
+		});
+		contribDiv.find(".reference_form > .metadata").html("");
 		jQuery.each(data.metadata, function(i, val) {
 
-			addMetadataField(val.name, val.tipo);
+			addMetadataField(contribDiv.find(".reference_form > .metadata"),
+				"reference[metadata]", val.name, val.tipo);
 
 		});
+		
+		updateForm(contribDiv.find(".newcontrib"), 
+			contribDiv.find(".reference_form"), "contribution[metadata]", data.target);
+
+		contribDiv.find(".tipoContrib").val(data.target.nombre);
+		contribDiv.find(".newcontrib").show();
+		contribDiv.find(".target").unbind('change');
+		contribDiv.find(".target").change(function(){
+			if(contribDiv.find(".target").val()=="-1")
+				contribDiv.find(".newcontrib").show();
+			else
+				contribDiv.find(".newcontrib").hide();
+
+
+		});
+		
+
 	 }, "json");
 
 
-});
+}
+
+
+
+
 
 
 });
